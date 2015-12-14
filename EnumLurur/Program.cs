6bf6de89a2a117a5f23e0db76d3fd02e319795e6
RamiGranat <raextheassassin@gmail.com>
2015-12-14 10:42:19 +0100

    Menu1

--- EnumLurur/Program.cs
+++ EnumLurur/Program.cs
@@ -15,46 +15,68 @@ namespace EnumLurum
 
         static void Main(string[] args)
         {
+            StartMathProgram();
+        }
+
+        private static void StartMathProgram()
+        {
             bool loopa = true;
             do
             {
                 Console.Clear();
-            PrintMenuWithEnums();
-                int tal;
-                if (int.TryParse(Console.ReadLine(), out tal))
-                {
-                    Menu menu = (Menu)tal;
-                    switch (menu)
-                    {
-                        case Menu.Add:
-                            Console.WriteLine(Menu.Add);
-                            break;
-                        case Menu.Subtract:
-                            Console.WriteLine(Menu.Subtract);
-                            break;
-                        case Menu.Multiply:
-                            Console.WriteLine(Menu.Multiply);
-                            break;
-                        case Menu.Divide:
-                            Console.WriteLine(Menu.Divide);
-                            break;
-                        case Menu.End:
-                            Console.WriteLine(Menu.End);
-                            loopa = false;
-                            break;
-                        default:
-                            //throw new ArgumentOutOfRangeException();
-                            Console.WriteLine("Something went wrong, please try again.");
-                            break;
-                    }
-                    System.Threading.Thread.Sleep(3000);
-                }
-                //Läs in ett tal
-                //Omvandla talet till en enum
-                //Switcha på enumet
-                //Vi får nog loopa detta
+                PrintMenuWithEnums();
+                loopa = MenuChoice(loopa);
             } while (loopa);
             Console.ReadLine();
+        }
+
+        private static bool MenuChoice(bool loopa)
+        {
+            int tal;
+            if (int.TryParse(Console.ReadLine(), out tal))
+            {
+                Menu menu = (Menu)tal;
+                switch (menu)
+                {
+                    case Menu.Add:
+                        AddMethod();
+                        break;
+                    case Menu.Subtract:
+                        Console.WriteLine(Menu.Subtract);
+                        break;
+                    case Menu.Multiply:
+                        Console.WriteLine(Menu.Multiply);
+                        break;
+                    case Menu.Divide:
+                        Console.WriteLine(Menu.Divide);
+                        break;
+                    case Menu.End:
+                        Console.WriteLine(Menu.End);
+                        loopa = false;
+                        break;
+                    default:
+                        //throw new ArgumentOutOfRangeException();
+                        Console.WriteLine("Something went wrong, please try again.");
+                        break;
+                }
+                System.Threading.Thread.Sleep(3000);
+            }
+            //Läs in ett tal
+            //Omvandla talet till en enum
+            //Switcha på enumet
+            //Vi får nog loopa detta
+            return loopa;
+        }
+
+        private static void AddMethod()
+        {
+            Console.WriteLine(Menu.Add);
+            int tal1, tal2;
+            Console.WriteLine("Skriv in två tal som du vill addera");
+            if (int.TryParse(Console.ReadLine(), out tal1) && int.TryParse(Console.ReadLine(), out tal2))
+            {
+                Console.WriteLine("{0}+{1}={2}", tal1, tal2, tal1 + tal2);
+            }
         }
 
         private static void PrintMenuWithEnums()
---
+++ EnumOvning/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOvning
+{
+    enum Menu
+    {
+        ett, tvau, avsluta
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopa = true;
+            do
+            {
+                PrintMenu();
+                loopa = MenuChoice(loopa);
+            } while (loopa);
+        }
+
+        private static void PrintMenu()
+        {
+            int counter = 0;
+            foreach (var item in Enum.GetNames(typeof(Menu)))
+            {
+                Console.WriteLine("{0}: {1}", counter++, item);
+            }
+        }
+
+        private static bool MenuChoice(bool loopa)
+        {
+            int tal;
+            if (int.TryParse(Console.ReadLine(), out tal))
+            {
+                Menu menu = (Menu)tal;
+                switch (menu)
+                {
+                    case (Menu.ett):
+                        Console.WriteLine(Menu.ett);
+                        break;
+                    case (Menu.tvau):
+                        Console.WriteLine(Menu.tvau);
+                        break;
+                    case (Menu.avsluta):
+                        Console.WriteLine(Menu.avsluta);
+                        loopa = false;
+                        break;
+                    default:
+                        Console.WriteLine("You wrote an invalid number, please try again.");
+                        break;
+                }
+            }
+                        return loopa;
+        }
+    }
+}
